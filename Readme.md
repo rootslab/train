5c2b1964c0aa0f0e4f2d24b1165bc5f3191cac23
Guglielmo Ferri <44gatti@gmail.com>
2013-01-09 00:20:56 +0100

    Hello folks! On branch master Initial commit Changes to be committed: new file:   Readme.md new file:   index.js new file:   lib/train.js new file:   package.json new file:   test/npm-debug.log new file:   test/run.sh new file:   test/train-test.js

---
+++ Readme.md
@@ -0,0 +1,48 @@
+###Train 
+[![build status](https://travis-ci.org/rootslab/train.png)](https://travis-ci.org/rootslab/train)
+> The Train module is an implementation of a Queue data structure.
+> It uses 2 arrays, to simulate and perform a fast element shift operation on its head, without using the Array#shift() method.
+
+###Install
+```bash
+$ npm install train [-g]
+```
+###Run Tests
+
+```javascript
+$cd train/
+$npm test
+```
+
+###Constructor
+
+> Create an instance, optionally with an Array of elements. 
+
+```javascript
+Train( [ Array elements ] )
+// or
+new Train( [ Array elements ] )
+```
+
+###Properties
+
+```javascript
+// a property to get the queue size
+// NOTE that accessors are very slow, use the size method
+Train.length : Number
+```
+
+###Methods
+
+```javascript
+// get an element at certain index
+Train#get( index ) : Object
+// pop the head element from queue
+Train#shift : Object
+// push an element to the queue head
+Train#push( el ) : Number
+// get the queue size/length
+Train#size : Number
+// reset queue
+Train#flush( index ) : Number
+```
---
+++ test/train-test.js
@@ -0,0 +1,60 @@
+var log = console.log,
+    assert = require( 'assert' ),
+    Train = require( '../' ),
+    arr = [ 
+        'Cane Nero',
+        'Lupus est agnum',
+        'Otia et negotia',
+        'Cave canem',
+        'Omnes feriunt',
+        'Mitto tibi navem sine poppa sine prora'
+    ],
+    el = null,
+    len = 0,
+    size = 0,
+    t = new Train();
+
+log( '- push 5 elements in the queue.' );
+
+t.push( arr[ 0 ] );
+t.push( arr[ 1 ] );
+t.push( arr[ 2 ] );
+t.push( arr[ 3 ] );
+t.push( arr[ 4 ] );
+
+log( '- pop/shift 3 elements from queue, check if elements match with original ones.' );
+el = t.shift();
+assert.equal( el, arr[ 0 ], '- Test failed! Elements don\'t match: ' + el + '!==' + arr[ 0 ] );
+el = t.shift();
+assert.equal( el, arr[ 1 ], '- Test failed! Elements don\'t match: ' + el + '!==' + arr[ 1 ] );
+el = t.shift();
+assert.equal( el, arr[ 2 ], '- Test failed! Elements don\'t match: ' + el + '!==' + arr[ 2 ] );
+
+log( '- test if the current queue length is 2.' );
+len = t.length;
+assert.equal( len, 2, '- Test failed! Returned queue length is wrong: 2 !== ' + len );
+log( '- test if the current queue.length is == queue.size().' );
+size = t.size()
+assert.equal( len, 2, '- Test failed! Returned queue size is different from length:' + size + '!== ' + len );
+
+
+log( '- test if get({index}) method returns correct elements.' );
+el = t.get( 0 );
+
+assert.equal( el, arr[ 3 ], '- Test failed! Elements don\'t match:' + el + '!== ' + arr[ 3 ] );
+
+el = t.get( 1 );
+assert.equal( el, arr[ 4 ], '- Test failed! Elements don\'t match:' + el + '!== ' + arr[ 4 ] );
+
+t.push( arr[ 5 ] );
+
+el = t.get( 2 );
+assert.equal( el, arr[ 5 ], '- Test failed! Elements don\'t match:' + el + '!== ' + arr[ 5 ] );
+
+log( '- flush the queue, then should be 0.' );
+t.flush();
+size = t.size();
+assert.equal( size, 0, '- Test failed! The queue should be empty, now its size is:' + size );
+log( '- queue.shift() should return an undefined value.' );
+el = t.shift();
+assert.equal( el, undefined, '- Test failed! The queue should be empty, shift should return an undefined value:' + el );
