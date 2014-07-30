9bc5ae1bf13404af03c641cddb0567276ae35f7c
rootslab <44gatti@gmail.com>
2014-07-30 07:25:41 +0200

    v3.4.1. Fixed test, removed Funny dependency. On branch master modified:   package.json modified:   test/push-test.js

--- test/push-test.js
+++ test/push-test.js
@@ -2,7 +2,6 @@
 
 var log = console.log
     , assert = require( 'assert' )
-    , Funny = require( 'funny' )
     , Train = require( '../' )
     , arr = [ 
         'Cane Nero',
@@ -14,8 +13,6 @@ var log = console.log
         ]
     ]
     , t = new Train()
-    // shortcut useful to use an array as arguments for a function
-    , push = Funny.mock( Train.prototype.push, true )
     ;
 
 log( '- test #push with 0 arguments, no item should be added.' );
@@ -23,8 +20,7 @@ log( '- check if returned value is %d', 0 );
 assert.equal( t.push(), 0, '#push return value is wrong!' );
 
 log( '- test #push with multiple arguments: %d', arr.length );
-// shortcut for: t.push.apply( t, arr )
-push( t, arr );
+t.push.apply( t, arr );
 
 log( '- check resulting queue length: %d', t.length );
 assert.ok( t.length, arr.length, 'queue size value is wrong!' );
